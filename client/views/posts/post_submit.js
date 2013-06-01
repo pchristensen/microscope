8af24bac67cdbe2a5fb3e33bd55363533c1fafbc
Peter Christensen <peter@pchristensen.com>
2013-05-31 18:02:02 -0700

    9-2 Integrate errors into postSubmit

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -9,10 +9,14 @@ Template.postSubmit.events({
         };
 
         Meteor.call('post',post, function(error, id) {
-            if (error)
-                return alert(error.reason);
+            if (error) {
+                throwError(error.reason);
 
-            Meteor.Router.to('postPage', id);
+                if (error.error === 302)
+                    Meteor.Router.to("postPage", error.details);
+            } else {
+                Meteor.Router.to('postPage', id);
+            }
         });
     }
 });
